6019031233a53b0efbe30cfb153f2fa6283ad7c0
Claudia Lam <claudiaslam@gmail.com>
2023-02-07 16:56:22 -0800

    refactor displayMeme function

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 function displayMeme(e) {
-  //stop the form from submitting
-  e.preventDefault();
-  createNewDiv();
+  e.preventDefault(); //stop the form from submitting
+  const newMemeContainer = createNewDiv();
+  const memeImg = createImg();
+  const memetext = createText(["top", "bottom"]);
+  const deleteBtn = createDeleteButton();
+  newMemeContainer.append(memeImg, deleteBtn, ...memetext);
+  deleteMeme();
+  deleteAllMemes();
   document.querySelector("form").reset();
 }
 
-const memeDisplay = document.querySelector(".meme-display");
-
 function createNewDiv() {
-  //add a new div to memeDisplay
+  const memeDisplay = document.querySelector(".meme-display");
   const newDiv = document.createElement("div");
   newDiv.classList.add("meme-div");
   memeDisplay.append(newDiv);
-  createImg(newDiv);
+  return newDiv;
 }
 
 function createImg(parentNode) {
-  //add a new image to new div
   const imgURL = document.getElementById("image-url").value;
   const newImg = document.createElement("img");
   newImg.src = `${imgURL}`;
   newImg.setAttribute("id", "meme-img");
-  parentNode.append(newImg);
-  //add text to image
-  createText("top", parentNode);
-  createText("bottom", parentNode);
-  createDeleteButton(parentNode);
-  //delete all memes
-  const allNewDivs = document.querySelectorAll(".meme-div");
-  deleteAllMemes(allNewDivs);
+  return newImg;
 }
 
-function createText(textPosition, parentNode) {
-  const memeText = document.getElementById(`${textPosition}-text`).value;
-  const newText = document.createElement("div");
-  newText.classList.add(`${textPosition}-text`);
-  newText.innerText = `${memeText}`;
-  parentNode.append(newText);
+function createText(textPositions) {
+  const textNodes = [];
+  textPositions.forEach((text) => {
+    const formText = document.getElementById(`${text}-text`).value;
+    const newText = document.createElement("div");
+    newText.classList.add(`${text}-text`);
+    newText.innerText = `${formText}`;
+    textNodes.push(newText);
+  });
+  return textNodes;
 }
 
-function createDeleteButton(parentNode) {
+function createDeleteButton() {
   const newSpan = document.createElement("span");
   newSpan.innerHTML = "&times;";
   newSpan.classList.add("close");
-  parentNode.append(newSpan);
-  const deleteBtn = document.querySelectorAll(".close");
-  deleteMeme(deleteBtn);
+  return newSpan;
 }
 
-function deleteMeme(deleteElement) {
+function deleteMeme() {
+  const deleteElement = document.querySelectorAll(".close");
   for (let i = 0; i < deleteElement.length; i++) {
     deleteElement[i].addEventListener("click", function () {
       deleteElement[i].parentElement.style.display = "none";
@@ -56,9 +54,9 @@ function deleteMeme(deleteElement) {
   }
 }
 
-function deleteAllMemes(allMemes) {
+function deleteAllMemes() {
+  const allMemesArr = document.querySelectorAll(".meme-div");
   const deleteAllBtn = document.querySelector("#delete-all");
-  const allMemesArr = Array.from(allMemes);
   deleteAllBtn.addEventListener("click", function () {
     for (const meme of allMemesArr) {
       meme.style.display = "none";
